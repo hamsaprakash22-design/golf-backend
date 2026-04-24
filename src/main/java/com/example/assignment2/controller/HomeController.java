8f1467ef602a3304d278041a2ddac7202e3ac74c
Hamsa Prakash <hamsaprakash22@gmail.com>
2026-04-24 15:18:34 +0530

    Added HomeController to fix whitelabel error

---
+++ src/main/java/com/example/assignment2/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.assignment2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "🚀 Golf Backend is Running Successfully!";
+    }
+}
